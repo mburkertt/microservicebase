2dcf6b0989aba3dee1594f2abac9122f861c355c
Matthias <matthiasburkerttriberg@googlemail.com>
2017-09-26 05:27:58 +0200

    reformate pom

--- src/integration-test/java/ch.erni.microservicebase/AbcServerApplicationIT.java
+++ src/integration-test/java/ch.erni.microservicebase/AbcServerApplicationIT.java
@@ -1,3 +1,6 @@
+package ch.erni.microservicebase;
+
+import ch.erni.microservicebase.ExampleApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/main/java/ch/erni/microservicebase/ExampleApplication.java
+++ src/main/java/ch/erni/microservicebase/ExampleApplication.java
@@ -1,3 +1,5 @@
+package ch.erni.microservicebase;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
